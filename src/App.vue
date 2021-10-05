95f5cc5b2d7987a32e3443f8f822d71d2a370d72
CarlosVilla25 <ing.cavij@gmail.com>
2021-10-05 12:11:55 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <counter :start="101"/> -->
+  <indecision />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Counter from "./components/Counter";
+import Indecision from './components/Indecision.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // Counter,
+    Indecision
   }
 }
 </script>
